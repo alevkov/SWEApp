0c53631e39ec56a5f1c861b52c96d35fde877a74
wdolce12 <wdolce12@gmail.com>
2017-04-04 11:24:13 -0400

    change profile view

--- app/src/main/java/layout/DashboardProfile.java
+++ app/src/main/java/layout/DashboardProfile.java
@@ -3,12 +3,25 @@ package layout;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.example.lexlevi.sweapp.Models.Course;
+import com.example.lexlevi.sweapp.Models.User;
 import com.example.lexlevi.sweapp.R;
+import com.example.lexlevi.sweapp.Singletons.ChatServerClient;
+import com.example.lexlevi.sweapp.Singletons.UserSession;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,6 +41,25 @@ public class DashboardProfile extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private String _username;
+    private String _firstName;
+    private String _lastname;
+    private String _major;
+    private String _year;
+    private String _email;
+    //private String _courses;
+
+
+
+    private View _rootView;
+    private TextView _usernameTextView;
+    private TextView _nameTextView;
+    private TextView _yearTextView;
+    private TextView _majorTextView;
+    private TextView _emailTextView;
+    private TextView _coursesTextView;
+
+
     private OnFragmentInteractionListener mListener;
 
     public DashboardProfile() {
@@ -41,12 +73,15 @@ public class DashboardProfile extends Fragment {
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -56,8 +91,42 @@ public class DashboardProfile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        _rootView = inflater.inflate(R.layout.fragment_dashboard_profile, container, false);
+        _username = UserSession.getInstance().getCurrentUser().getUserName();
+        _firstName = UserSession.getInstance().getCurrentUser().getFirstName();
+        _lastname = UserSession.getInstance().getCurrentUser().getLastName();
+        _year = UserSession.getInstance().getCurrentUser().getYear();
+        _email = UserSession.getInstance().getCurrentUser().getEmail();
+        _major = UserSession.getInstance().getCurrentUser().getMajor();
+        //_courses = UserSession.getInstance().getCurrentUser().getCourses();
+
+
+        _usernameTextView = (TextView) _rootView.findViewById(R.id.username_profile);
+        _nameTextView = (TextView) _rootView.findViewById(R.id.name_profile) ;
+        _yearTextView = (TextView) _rootView.findViewById(R.id.profile_year);
+        _emailTextView = (TextView) _rootView.findViewById(R.id.profile_email);
+        _majorTextView = (TextView) _rootView.findViewById(R.id.profile_major) ;
+        _coursesTextView = (TextView) _rootView.findViewById(R.id.profile_courses);
+
+        String courses = "";
+        _usernameTextView.setText(_username);
+        _nameTextView.setText(_firstName + " " + _lastname);
+        _yearTextView.setText("( "+_year+" )");
+        _emailTextView.setText("E-mail: " +_email);
+        _majorTextView.setText("Major: " + _major);
+
+        //courses += "Courses: \r\n";
+
+        for (Course c : UserSession.getInstance().getCurrentUser().getCourses()) {
+            courses += c.getName();
+            courses += "\r\n";
+        }
+
+        _coursesTextView.setText(courses);
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard_profile, container, false);
+        return _rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
