055e260e24a3d7d0effa9181f28bac7e6110c14a
Aleksey <sphota9@gmail.com>
2017-04-23 17:03:12 -0400

    add more event endpoints to api interface

--- app/src/main/java/com/example/lexlevi/sweapp/Common/URLs.java
+++ app/src/main/java/com/example/lexlevi/sweapp/Common/URLs.java
@@ -34,4 +34,9 @@ public class URLs {
     public static final String kUpdateChatForId = "chats/{id}/update";
     public static final String kDeleteChatForId = "chats/{id}/delete";
     public static final String kCreateNewMessageForChat = "chats/{id}/messages/new";
+
+    public static final String kGetEventsForGroup = "events/group/{groupId}";
+    public static final String kCreateNewEventForGroup = "events/{groupId}/new";
+    public static final String kUpdateEventForId = "events/{id}/update";
+    public static final String kDeleteEventForId = "events/{id}/delete";
 }
